875c4cdccaeaef00af29dca9e4ec32af98120c4e
Jay Pfaffman <jay@literatecomputing.com>
2025-11-04 13:27:16 -0600

    add htmlSafe test

---
+++ spec/system/text_block_html_safe_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative "page_objects/components/text_block_component"
+
+RSpec.describe "Text block HTML-safe rendering", type: :system do
+  fab!(:group1) { Fabricate(:group, name: "group1") }
+  fab!(:member) { Fabricate(:user, groups: [group1]) }
+  let!(:theme) { upload_theme_component }
+
+  def configure_text_block(entry)
+    theme.update_setting(:custom_text_block, [entry])
+    theme.save!
+  end
+
+  it "renders HTML tags unescaped when the text contains HTML" do
+    configure_text_block(
+      {
+        "name" => "text_block html",
+        "text" => "This is <strong>bold</strong> text",
+        "outlet" => "top-notices",
+        "class" => "test-text-block",
+        "group_action" => "show",
+        "groups" => [group1.id]
+      }
+    )
+
+    sign_in(member)
+    visit "/"
+
+    # The component should render the <strong> tag, not escape it.
+    expect(page).to have_css('.custom-component strong', text: 'bold')
+  end
+end
